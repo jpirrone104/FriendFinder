34b6af78500250b90d21d63de1d2b3bcb6d195a6
Jen Pirrone <jen.pirrone@clarivate.com>
2019-02-19 20:25:11 -0500

    Adding routing issue

--- app/routing/apiroutes.js
+++ app/routing/apiroutes.js
@@ -1,51 +1,57 @@
 
 var friendsData = require("../data/friends");
+var express = require("express");
+
+
+var app = express();
 
 module.exports = function(app) {
     app.get("/api/friends", function(req, res) {
         res.json(friendsData);
       });
-    
+    };
 
-    app.post('/api/friends', function(req, res) {
-      // Capture the user input object
-      var surveyData = req.body;
-      // console.log('userInput = ' + JSON.stringify(userInput));
-  
-      var surveyResponses = surveyData.scores;
-      // console.log('userResponses = ' + userResponses);
-  
-      // Compute best friend match
-      var matchName = '';
-      var matchImage = '';
-      var totalDifference = 10000; 
-  
-      // Examine all existing friends in the list
-      for (var i = 0; i < friendArray.length; i++) {
-        // console.log('friend = ' + JSON.stringify(friends[i]));
-  
-        // Compute differenes for each question
-        var diff = 0;
-        for (var j = 0; j < surveyResponses.length; j++) {
-          diff += Math.abs(friendArray[i].scores[j] - surveyResponses[j]);
-        }
-        // console.log('diff = ' + diff);
-  
-        // If lowest difference, record the friend match
-        if (diff < totalDifference) {
-          // console.log('Closest match found = ' + diff);
-          // console.log('Friend name = ' + friends[i].name);
-          // console.log('Friend image = ' + friends[i].photo);
-  
-          totalDifference = diff;
-          matchName = friendArray[i].name;
-          matchImage = friendArray[i].photo;
-        }
-      }
-  
-      // Add new user
-      friendArray.push(surveyData);
+    app.post("/api/friends", function (req, res) {
+      // surveyData is the user that filled out the survey
+    //   var surveyData = req.body;
+
+    //   // compute best match from scores
+    //   var newMatch = {};
+
+    //   for(var i = 0; i < surveyData.scores.length; i++) {
+    //     if(surveyData.scores[i] == "1 (Strongly Disagree)") {
+    //       surveyData.scores[i] = 1;
+    //     } else if(surveyData.scores[i] == "5 (Strongly Agree)") {
+    //       surveyData.scores[i] = 5;
+    //     } else {
+    //       surveyData.scores[i] = parseInt(surveyData.scores[i]);
+    //     }
+    //   }
+
+    //  var newMatchIndex = 0;
+    //  var newMatchDifference = 40;
+
+    //  for(var i = 0; i < friendsData.length; i++) {
+    //    var totalDifference = 0;
+
+    //    for(var j = 0; j < friendsData[i].scores.length; j++) {
+    //      var differenceScore = Math.abs(friendsData[i].scores[j] - surveyData.scores[j]);
+
+    //      totalDifference += differenceScore;
+    //    }
+
+    //    if(totalDifference < newMatchDifference) {
+    //      newMatchIndex = i;
+    //      newMatchDifference = totalDifference;
+    //    }
+    //  }
+   
+    //  newMatch = friendsData[newMatchIndex];
+
+    //  friendsData.push(surveyData);
+
+     res.json(true);
 
     });
-  };
+
   
